6ff0c1b4aef3b37aaa34ae6d3be689e6c1da47bf
Reyhan Balthazar Epsa <reyhanbalthazarepsa@gmail.com>
2026-02-28 12:58:49 +0700

    prod

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Create an axios instance
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 const api = axios.create({
   baseURL: API_BASE_URL,
